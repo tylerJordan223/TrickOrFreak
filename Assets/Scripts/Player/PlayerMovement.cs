e9f3a45238caf1ab7636159a5d5af674ab067bca
tylerJordan223 <tjlinkmaster22@gmail.com>
2025-10-31 22:03:07 -0400

    Fixed Player Movement (drag)

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,14 +6,22 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
     public float speed;
+    public float ground_drag;
+    private float adjusted_speed;
 
     public Transform orientation;
 
     float horizontalInput;
     float verticalInput;
+    public float gravityMultiplier;
 
     Vector3 direction;
     Rigidbody rb;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask ground;
+    bool grounded;
 
     private void Start()
     {
@@ -23,6 +31,9 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        //ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight + 0.2f, ground);
+
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
@@ -32,11 +43,37 @@ public class PlayerMovement : MonoBehaviour
             Vector3 limitedVel = flatVel.normalized * speed;
             rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
         }
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            adjusted_speed = speed * 2;
+        }
+        else
+        {
+            adjusted_speed = speed;
+        }
+
+        if(grounded)
+        {
+            rb.drag = ground_drag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
+        Debug.Log(grounded);
     }
 
     private void FixedUpdate()
     {
         direction = orientation.forward * verticalInput + orientation.right * horizontalInput;
-        rb.AddForce(direction.normalized * speed * 10f, ForceMode.Force);
+        if(grounded)
+        {
+            rb.AddForce(direction.normalized * adjusted_speed * 10f, ForceMode.Force);
+        }
+        else
+        {
+            rb.AddForce(direction.normalized * adjusted_speed * 10f * gravityMultiplier, ForceMode.Force);
+        }
     }
 }
